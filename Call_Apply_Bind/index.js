cfbb57089744d5bae5abd24155b962a6763221e2
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-21 12:16:24 +0530

    complete

--- Call_Apply_Bind/index.js
+++ Call_Apply_Bind/index.js
@@ -1,7 +1,16 @@
 var obj  = {num:2};
 
-var addToThis = function(a) {
-    return this.num + a;
+var addToThis = function(a, b, c) {
+    return this.num + a + b + c;
 };
 
-console.log(addToThis.call(obj, 3)); // functionName.call(obj, functionArguments)
+// call
+console.log(addToThis.call(obj, 3, 1, 2)); // functionName.call(obj, functionArguments)
+
+// apply
+var arr = [1,2,3];
+console.log(addToThis.apply(obj, arr)); 
+
+// bind
+var bound = addToThis.bind(obj);
+console.log(bound(1,2,3));

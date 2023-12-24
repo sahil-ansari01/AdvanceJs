f51cd597a2c090444ba12fd32466485aa9abfcc1
sahil-ansari01 <opensourcecarrier@gmail.com>
2023-12-24 12:20:56 +0530

    Event Loop

---
+++ Event_Loop/main.js
@@ -0,0 +1,12 @@
+//  while isNotEmpty(eventQueue)
+//       pull out first item from the event queue
+//   execute it
+
+
+// while isNotEmpty(eventQueue)
+// pull out first item from event queue
+// follow the execution logic until call stack empty
+
+// let order1 = takeOrder(table1);
+// chef.handOver (order, dish => serveTable (dish));
+// let order2 = takeOrder(table2);
